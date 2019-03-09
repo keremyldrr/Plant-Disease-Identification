9891426976153698ce77216ee1d579eae8b8642d
Baris Sevilmis <barissevilmis@Bariss-MacBook-Pro.local>
2019-03-09 18:33:30 +0300

    Restful-API

--- app/server.py
+++ app/server.py
@@ -1,25 +1,34 @@
 import os
 import numpy as np
 import pprint
+import cv2 as cv
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 from flask import Flask
+import json
 from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
+
+class Plant(Resource):
+    def get(self):
+        return json.dumps("Please enter image path")
+
 class Plant_Disease(Resource):
     def get(self,image_path=""):
-        print(image_path)
+        #print(image_path)
         if image_path!="favicon.ico":
             img = plt.imread(image_path[:])
-            img = np.reshape(img,[224,224,3])
-            img = np.expand_dims(img,axis=0)
-        
+            #print(img.shape)
+            img = np.reshape(img,(256,256,3))
+            img = np.expand_dims(img,axis=0)        
+            print(np.argmax(model.predict(img)))
             try:
                 pred = np.argmax(model.predict(img))
             except:
-                return "Bad image path"
+                return json.dumps("Bad image path")
+
             return dicty[pred]
         return ""
 
@@ -42,6 +51,7 @@ json_file.close()
 model = tf.keras.models.model_from_json(loaded_model_json)
 model.load_weights("weights.h5")
 
+api.add_resource(Plant,'/')
 api.add_resource(Plant_Disease, '/<string:image_path>')
 
 if __name__ == '__main__':
