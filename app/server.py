4ff60f7b9a87e1d47af042a132eea414e34df56c
keremyldrr <keremyildirir@sabanciuniv.edu>
2019-03-12 15:00:23 +0300

    oldu gibi

--- app/server.py
+++ app/server.py
@@ -8,35 +8,8 @@ import matplotlib.pyplot as plt
 from flask import Flask
 import json
 from flask_restful import reqparse, abort, Api, Resource
-app = Flask(__name__)
-api = Api(app)
-
-class Plant(Resource):
-    def get(self):
-        return json.dumps("Please enter image path")
-
-class Plant_Disease(Resource):
-    def get(self,image_path=""):
-        #print(image_path)
-        if image_path!="favicon.ico":
-            json_file = open('model.json', 'r')
-            loaded_model_json = json_file.read()
-            json_file.close()
-            model = tf.keras.models.model_from_json(loaded_model_json)
-            model.load_weights("weights.h5")
-            img = plt.imread(image_path[:])
-            #print(img.shape)
-            img = np.resize(img,(256,256,3))
-            img = np.expand_dims(img,axis=0)        
-            pred = np.argmax(model.predict(img))
-            try:
-                pred = np.argmax(model.predict(img))
-            except:
-                return json.dumps("Bad image path")
-
-            return dicty[pred]
-        return ""
-
+from flaskext.mysql import MySQL
+import urllib
 dicty = {0:'blight' ,
          1:'greening',
          2: 'healthy',
@@ -48,15 +21,45 @@ dicty = {0:'blight' ,
          8:'scorch',
          9:'spot',
          10:'virus'}
+app = Flask(__name__)
+api = Api(app)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'plantid_user'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'lvFu3&48rd#s'
+app.config['MYSQL_DATABASE_DB'] = 'plantid'
+app.config['MYSQL_DATABASE_HOST'] = '10.36.48.64'
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
+class check(Resource):
+    def get(self):
+        cursor.execute("SELECT id,path_web FROM disease_job WHERE status=0")
+        all_data = cursor.fetchall()
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = tf.keras.models.model_from_json(loaded_model_json)
+        model.load_weights("weights.h5")
+        for data in all_data:
+            path = urllib.request.urlopen( "http://plantid.sabanciuniv.edu/"+data[1])
+            img = plt.imread(path,format='jpg')#format check, error handling
+           
+            img = np.resize(img,(256,256,3))
+            img = np.expand_dims(img,axis=0)        
+            pred = np.argmax(model.predict(img))
+            print(dicty[pred])
+            query = "UPDATE disease_job SET result='"+dicty[pred]+"',status=2 WHERE id="+str(data[0])
+            print(query)
+            cursor.execute(query)
 
+        return json.dumps({"success"}),200
+
+api.add_resource(check,"/identify_disease")
+app.run(debug=False)
         
 
 
-api.add_resource(Plant,'/')
-api.add_resource(Plant_Disease, '/<string:image_path>')
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
         
